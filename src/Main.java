4b950501bdba0b7f30eedb7633db044c7bbbfe0b
n-stepanova <nstepanovaqa@gmail.com>
2023-09-04 20:41:50 +0300

    returning strings

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static String greet(String name)
+    {
+        return "Hello, " + name + " how are you doing today?";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("Tony"));
+    }
+}
